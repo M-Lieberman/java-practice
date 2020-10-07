5c6d3d2bb4b30ace5869ecf90d62fd8454d3d613
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-07 18:51:58 +0100

    initial commit

---
+++ src/test/java/com/codewars/TvRemoteTest.java
@@ -0,0 +1,14 @@
+package com.codewars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TvRemoteTest {
+
+    @Test
+    public void testTvRemote() {
+        fail("not implemented");
+    }
+
+}
