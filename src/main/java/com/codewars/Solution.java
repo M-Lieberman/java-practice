0891ebf72263d241f51eaa92267170e66d9965d0
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-09 08:50:19 +0000

    add Fix String Case puzzle

---
+++ src/main/java/com/codewars/Solution.java
@@ -0,0 +1,48 @@
+package com.codewars;
+
+/**
+ * https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/java
+ * In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+ *
+ * make as few changes as possible.
+ * if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+ * For example:
+ *
+ * solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+ * solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+ * solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+ */
+public class Solution {
+    public static String solve(final String str) {
+        int upper = 0;
+        int lower = 0;
+        char[] chars = str.toCharArray();
+        for (char c: chars) {
+            if (Character.isUpperCase(c)) {
+                upper++;
+            } else if (Character.isLowerCase(c)) {
+                lower++;
+            }
+        }
+        if (lower >= upper) {
+            for (int i = 0; i<chars.length ; i++) {
+                if (Character.isUpperCase(chars[i])) {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                }
+            }
+        } else {
+            for (int i = 0; i<chars.length ; i++) {
+                if (Character.isLowerCase(chars[i])) {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                }
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    public static String solveWithFilter(final String str) {
+        return str.chars().filter(Character::isLowerCase).count() >= str.chars().filter(Character::isUpperCase).count()
+                ? str.toLowerCase()
+                : str.toUpperCase();
+    }
+}
